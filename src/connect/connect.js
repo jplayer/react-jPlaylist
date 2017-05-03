ddfcc5a7f22bf64d4612cfc3e0807d3189a6181c
Martin Dawson <martino1995@blueyonder.co.uk>
2017-05-03 16:34:02 +0100

    Made connect simpler

--- src/connect/connect.js
+++ src/connect/connect.js
@@ -1,39 +1,14 @@
 import { connect } from 'react-redux';
 import { actions as jPlayerActions } from 'react-jplayer';
 import { connect as jPlayerConnect } from 'react-jplayer-utils';
-import getConnectedJPlayers from 'react-jplayer/src/util/getConnectedJPlayers';
-import merge from 'lodash.merge';
 
 import * as jPlaylistActions from '../actions/actions';
-import getConnectedJPlaylists from '../util/getConnectedJPlaylists';
-
-const mapStateToProps = ({ jPlayers, jPlaylists }, { id, ...props }) => {
-  const newJPlaylists = merge({},
-    getConnectedJPlayers(jPlayers),
-    getConnectedJPlaylists(jPlaylists),
-  );
-  const { [id]: jPlaylist, ...otherJPlayers } = newJPlaylists;
-
-  const returnedJPlaylist = {
-    ...props,
-    ...jPlaylist,
-    id,
-  };
-
-  if (Object.keys(otherJPlayers).some(otherJPlayer => otherJPlayer)) {
-    returnedJPlaylist.jPlayers = otherJPlayers;
-  }
-
-  return returnedJPlaylist;
-};
-
-const mapDispatchToProps = {
-  ...jPlayerActions,
-  ...jPlaylistActions,
-};
 
 const Connect = (jPlaylist, options, playlistOptions) => {
-  const ConnectedPlaylist = connect(mapStateToProps, mapDispatchToProps)(jPlaylist);
+  const ConnectedPlaylist = connect(state => state, {
+    ...jPlayerActions,
+    ...jPlaylistActions,
+  })(jPlaylist);
 
   return jPlayerConnect(jPlaylist, { options, playlistOptions }, ConnectedPlaylist);
 };

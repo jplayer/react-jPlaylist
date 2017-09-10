29f88506e4e70d99273221fc7145d47269ef99c0
Martin Dawson <u1356770@gmail.com>
2017-09-10 14:59:43 +0100

    Changed src to lib for main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initializeOptions from './initializeOptions/initializeOptions';
 import reducer from './reducer/reducer';
 import * as actions from './actions/actions';
+
+// Containers
 import JPlaylist from './components/jPlaylist/jPlaylistContainer';
 import Playlist from './components/playlist/playlistContainer';
 import Shuffle from './components/shuffle/shuffleContainer';
@@ -13,7 +15,28 @@ import MediaLink from './components/mediaLink/mediaLinkContainer';
 import Poster from './components/poster/posterContainer';
 import Title from './components/title/titleContainer';
 
+// Components
+import JPlaylistComponent from './components/jPlaylist/jPlaylist';
+import PlaylistComponent from './components/playlist/playlist';
+import ShuffleComponent from './components/shuffle/shuffle';
+import PreviousComponent from './components/previous/previous';
+import NextComponent from './components/next/next';
+import TogglePlaylistComponent from './components/togglePlaylist/togglePlaylist';
+import RemoveComponent from './components/remove/remove';
+import MediaLinkComponent from './components/mediaLink/mediaLink';
+
 export default JPlaylist;
+
+export {
+  JPlaylistComponent,
+  PlaylistComponent,
+  ShuffleComponent,
+  PreviousComponent,
+  NextComponent,
+  TogglePlaylistComponent,
+  RemoveComponent,
+  MediaLinkComponent,
+};
 
 export {
   initializeOptions,
